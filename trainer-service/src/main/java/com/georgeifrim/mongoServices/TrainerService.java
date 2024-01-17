65603f80ae2a8bf0354749320af941fb03838d1b
George Ifrim <ifrim.george.alexandru@gmail.com>
2024-01-17 13:08:23 +0200

    Enabled the transaction management for MongoDb, Added a service method that adds a trainer in DB

--- trainer-service/src/main/java/com/georgeifrim/mongoServices/TrainerService.java
+++ trainer-service/src/main/java/com/georgeifrim/mongoServices/TrainerService.java
@@ -4,13 +4,17 @@ import com.georgeifrim.mappers.TrainerMapper;
 import com.georgeifrim.mongoEntities.Trainer;
 import com.georgeifrim.mongoRepositories.TrainerRepo;
 import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
 public class TrainerService {
 
     private final TrainerRepo trainerRepo;
+
+    private final MongoTemplate mongoTemplate;
 
     public TrainerMapper trainerFromMongo(String firstName){
         Trainer trainer = trainerRepo.findByUserFirstName(firstName).orElseThrow(() -> new IllegalArgumentException("No such trainer exists!"));
@@ -19,6 +23,11 @@ public class TrainerService {
         TrainerMapper trainerMapper = mapper.toTrainerMapper(trainer);
 
         return trainerMapper;
+    }
+
+    @Transactional
+    public Trainer addMongoTrainer(Trainer trainer){
+        return mongoTemplate.insert(trainer);
     }
 
 }
